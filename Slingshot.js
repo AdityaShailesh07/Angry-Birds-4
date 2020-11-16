65c2b52abcb78c182aa54319f7090a20d3179232
AdityaShailesh07 <adityashalesh31@gmail.com>
2020-11-16 18:00:14 +0530

    have a good day

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        this.image1=loadImage("sprites/sling1.png");
+        this.image2=loadImage("sprites/sling2.png");
+        this.image3=loadImage("sprites/sling3.png");
+
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,11 +20,37 @@ class SlingShot{
     }
 
     display(){
+        image(this.image1,200,20);
+        image(this.image2,173,20)
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+
+
+            push();
+                    stroke(84,39,15)
+        if(pointA.x<220){
+            strokeWeight(7);
+                    
+            image(this.image3,pointA.x-20,pointA.y-15,50,30)
+            line(pointA.x-20, pointA.y, pointB.x, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+
+        }
+        else{
+
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+            image(this.image3,pointA.x-20,pointA.y-15,50,30)
+            line(pointA.x-20, pointA.y, pointB.x, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+        }
+
+
+            
+            
+            pop();
         }
     }
     
